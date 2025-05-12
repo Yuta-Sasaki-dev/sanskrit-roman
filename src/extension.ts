999bc5bcbbd1de9bd002c178140e746cbe7b70b0
Yuta-Sasaki-dev <ysasaki.smrti@gmail.com>
2025-05-12 23:53:25 +0900

    partially complete

--- src/extension.ts
+++ src/extension.ts
@@ -4,72 +4,79 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const replacementMap = new Map<string, string>([
 		['=a', 'ā'],
-		['=A', 'Ā'],
-		['=i', 'ī'],
-		['=I', 'Ī'],
-		['=u', 'ū'],
-		['=U', 'Ū'],
-		[',r', 'r̥'],
-		['=r', 'r̥̄'],
-		[',R', 'R̥'],
-		['=R', 'R̥̄'],
-		[',l', 'l̥'],
-		[',n', 'ṅ'],
-		[',N', 'Ṅ'],
-		['~n', 'ñ'],
-		['~N', 'Ñ'],
-		['.t', 'ṭ'],
-		['.T', 'Ṭ'],
-		['.d', 'ḍ'],
-		['.D', 'Ḍ'],
-		['.n', 'ṇ'],
-		['.N', 'Ṇ'],
-		[',s', 'ś'],
-		[',S', 'Ś'],
-		['.s', 'ṣ'],
-		['.S', 'Ṣ'],
-		['.m', 'ṃ'],
-		['.h', 'ḥ'],
-		[',m', 'm̐'],
-		['.l', 'ḷ'],
+		['\=A', 'Ā'],
+		['\=i', 'ī'],
+		['\=I', 'Ī'],
+		['\=u', 'ū'],
+		['\=U', 'Ū'],
+		['\,r', 'r̥'],
+		['\=r', 'r̥̄'],
+		['\,R', 'R̥'],
+		['\=R', 'R̥̄'],
+		['\,l', 'l̥'],
+		['\,n', 'ṅ'],
+		['\,N', 'Ṅ'],
+		['\~n', 'ñ'],
+		['\~N', 'Ñ'],
+		['\.t', 'ṭ'],
+		['\.T', 'Ṭ'],
+		['\.d', 'ḍ'],
+		['\.D', 'Ḍ'],
+		['\.n', 'ṇ'],
+		['\.N', 'Ṇ'],
+		['\,s', 'ś'],
+		['\,S', 'Ś'],
+		['\.s', 'ṣ'],
+		['\.S', 'Ṣ'],
+		['\.m', 'ṃ'],
+		['\.h', 'ḥ'],
+		['\,m', 'm̐'],
+		['\.l', 'ḷ'],
 		[':root:', '√']
 	]);
 	// Marker for target of transcript
 	const marker = /%skt\{(.*?)\}/g;
 
-	const udatta = /,\(\p{L}\p{M}?\)/gu;
-	const svarita = /_\(\p{L}\p{M}?\)/gu;
+	const udatta = /,?\(([a-zA-Z]\p{M}?)\)/gu;
+	const svarita = /_?\(([a-zA-Z]\p{M}?)\)/gu;
 
 	// Add accentuation for vedic texts
 	const accentuation = (text: string, regex: RegExp) => {
 		const accentType = (regexName: RegExp) => {
-			switch (regexName) {
-				case udatta:
-					return "\u0301";
-				case svarita:
-					return "\u0300";
-				default:
-					break;
+			if (regexName.source === udatta.source) {
+				return "\u0301";
 			}
+			if (regexName.source === svarita.source) {
+				return "\u0300";
+			}
+			return "";
 		};
-		const replacedText = text.replace(regex, (match, baseLetter) => {
-			return baseLetter + accentType(regex);
+		const replacedText = text.replace(regex, (match, capturedLetter) => {
+			return capturedLetter + accentType(regex);
 		});
 		return replacedText;
 	};
 
 	// Basic transcript process
 	const sanskritTranscript = (text: string) => {
-		let processedText = "";
+		// fundamental replacement logic
+		let processedText = text;
 		for (const [key, value] of replacementMap) {
-			const regexKey = new RegExp(`/${key}/gu`);
-			processedText = text?.replace(regexKey, value);
-		}
-		if (udatta.test(processedText)) {
-			processedText = accentuation(processedText, udatta);
-		} else if (svarita.test(processedText)) {
-			processedText = accentuation(processedText, svarita);
-		}
+			const escapedKey = key.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+			const regexKey = new RegExp(escapedKey, 'g');
+			processedText = processedText.replace(regexKey, value);
+		};
+		let textBeforeAccents = "";
+		do {
+			textBeforeAccents = processedText;
+			// accentuation logic
+			if (udatta.test(processedText)) {
+				processedText = accentuation(processedText, udatta);
+			}
+			if (svarita.test(processedText)) {
+				processedText = accentuation(processedText, svarita);
+			}
+		} while (textBeforeAccents !== processedText);
 		return processedText;
 	};
 
@@ -95,14 +102,14 @@ export function activate(context: vscode.ExtensionContext) {
 				);
 				editApply.replace(fullRangeText, processedText);
 			})
-			.then(success => {
-				if (success) {
-					vscode.window.setStatusBarMessage('Sanskrit Romanization Applied.');
-				} else {
-					console.error('Text replacement failed.');
-					vscode.window.showErrorMessage('Failed to apply Sanskrit romanization.');
-				}
-			});
+				.then(success => {
+					if (success) {
+						vscode.window.setStatusBarMessage('Sanskrit Romanization Applied.');
+					} else {
+						console.error('Text replacement failed.');
+						vscode.window.showErrorMessage('Failed to apply Sanskrit romanization.');
+					}
+				});
 		} else {
 			vscode.window.showInformationMessage('No Active Editor found.');
 		}
