fcf950ebb9ccc7af233a1940ebb9981becf5dc36
Yuta-Sasaki-dev <ysasaki.smrti@gmail.com>
2025-05-13 23:15:13 +0900

    full features developed

--- src/extension.ts
+++ src/extension.ts
@@ -4,57 +4,60 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const replacementMap = new Map<string, string>([
 		['=a', 'ā'],
-		['\=A', 'Ā'],
-		['\=i', 'ī'],
-		['\=I', 'Ī'],
-		['\=u', 'ū'],
-		['\=U', 'Ū'],
-		['\,r', 'r̥'],
-		['\=r', 'r̥̄'],
-		['\,R', 'R̥'],
-		['\=R', 'R̥̄'],
-		['\,l', 'l̥'],
-		['\,n', 'ṅ'],
-		['\,N', 'Ṅ'],
-		['\~n', 'ñ'],
-		['\~N', 'Ñ'],
-		['\.t', 'ṭ'],
-		['\.T', 'Ṭ'],
-		['\.d', 'ḍ'],
-		['\.D', 'Ḍ'],
-		['\.n', 'ṇ'],
-		['\.N', 'Ṇ'],
-		['\,s', 'ś'],
-		['\,S', 'Ś'],
-		['\.s', 'ṣ'],
-		['\.S', 'Ṣ'],
-		['\.m', 'ṃ'],
-		['\.h', 'ḥ'],
-		['\,m', 'm̐'],
-		['\.l', 'ḷ'],
+		['=A', 'Ā'],
+		['=i', 'ī'],
+		['=I', 'Ī'],
+		['=u', 'ū'],
+		['=U', 'Ū'],
+		[',r', 'r̥'],
+		['=r', 'r̥̄'],
+		[',R', 'R̥'],
+		['=R', 'R̥̄'],
+		[',l', 'l̥'],
+		[',n', 'ṅ'],
+		[',N', 'Ṅ'],
+		['~n', 'ñ'],
+		['~N', 'Ñ'],
+		['.t', 'ṭ'],
+		['.T', 'Ṭ'],
+		['.d', 'ḍ'],
+		['.D', 'Ḍ'],
+		['.n', 'ṇ'],
+		['.N', 'Ṇ'],
+		[',s', 'ś'],
+		[',S', 'Ś'],
+		['.s', 'ṣ'],
+		['.S', 'Ṣ'],
+		['.m', 'ṃ'],
+		['.h', 'ḥ'],
+		[',m', 'm̐'],
+		['.l', 'ḷ'],
 		[':root:', '√']
 	]);
 	// Marker for target of transcript
 	const marker = /%skt\{(.*?)\}/g;
 
-	const udatta = /,?\(([a-zA-Z]\p{M}?)\)/gu;
-	const svarita = /_?\(([a-zA-Z]\p{M}?)\)/gu;
+	// Marker for accentuation
+	const udatta = /,\((\p{L}\p{M}?)\)/gu;
+	const svarita = /_\((\p{L}\p{M}?)\)/gu;
 
 	// Add accentuation for vedic texts
+	const accentType = (regexName: RegExp) => {
+		if (regexName.source === udatta.source) {
+			return "\u0301";
+		} else if (regexName.source === svarita.source) {
+			return "\u0300";
+		}
+		return "";
+	};
 	const accentuation = (text: string, regex: RegExp) => {
-		const accentType = (regexName: RegExp) => {
-			if (regexName.source === udatta.source) {
-				return "\u0301";
-			}
-			if (regexName.source === svarita.source) {
-				return "\u0300";
-			}
-			return "";
-		};
-		const replacedText = text.replace(regex, (match, capturedLetter) => {
-			return capturedLetter + accentType(regex);
+		const accentedChar = accentType(regex);
+		if (!accentedChar) {
+			return text;
+		}
+		return text.replace(regex, (match, capturedLetter) => {
+			return capturedLetter + accentedChar;
 		});
-		return replacedText;
 	};
 
 	// Basic transcript process
@@ -66,9 +69,9 @@ export function activate(context: vscode.ExtensionContext) {
 			const regexKey = new RegExp(escapedKey, 'g');
 			processedText = processedText.replace(regexKey, value);
 		};
-		let textBeforeAccents = "";
+		let textBeforeAccentsA = "";
 		do {
-			textBeforeAccents = processedText;
+			textBeforeAccentsA = processedText;
 			// accentuation logic
 			if (udatta.test(processedText)) {
 				processedText = accentuation(processedText, udatta);
@@ -76,14 +79,23 @@ export function activate(context: vscode.ExtensionContext) {
 			if (svarita.test(processedText)) {
 				processedText = accentuation(processedText, svarita);
 			}
-		} while (textBeforeAccents !== processedText);
+			udatta.lastIndex = 0;
+		} while (textBeforeAccentsA !== processedText);
+		let textBeforeAccentsB = "";
+		do {
+			textBeforeAccentsB = processedText;
+			// accentuation logic
+			if (svarita.test(processedText)) {
+				processedText = accentuation(processedText, svarita);
+			}
+			svarita.lastIndex = 0;
+		} while (textBeforeAccentsA !== processedText);
 		return processedText;
 	};
 
 	const validateText = (input: string) => {
 		const output = input.replace(marker, (match, textContent) => {
-			const validText = sanskritTranscript(textContent);
-			return validText;
+			return sanskritTranscript(textContent);
 		});
 		return output;
 	};
